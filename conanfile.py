a5d336bc2541bc9275b4409c0baff506caf74b5a
Dmitry Bely <d.bely@recognize.ru>
2018-03-27 14:33:19 +0300

    Fix settings handling

--- conanfile.py
+++ conanfile.py
@@ -7,10 +7,7 @@ class JWasmConan(ConanFile):
     description = "JWasm Masm-compatible assembler"
     url = "http://github.com/db4/conan-jwasm"
     license = "Sybase Open Watcom Public License"
-    settings = "os", "compiler", "os_build", "arch", "arch_build"
-    # if os is not defined, conan passes CMAKE_SYSTEM_NAME="Generic"
-    # no cmake during x86_64 -> x86 cross building and cmake
-    # fails to configure Visual Studio project correctly
+    settings = "os", "compiler", "arch"
     exports_sources = "CMakeLists.txt"
     generators = "cmake"
 
@@ -31,11 +28,10 @@ class JWasmConan(ConanFile):
         self.env_info.path.append(os.path.join(self.package_folder, "bin"))
 
     def package_id(self):
-        os = self.info.settings.os
-        arch = self.info.settings.arch
+        self.info.include_build_settings()
+        self.info.settings.arch_build = self.info.settings.arch
+        self.info.settings.os_build = self.info.settings.os
         del self.info.settings.os
         del self.info.settings.compiler
         del self.info.settings.arch
-        self.info.settings.os_build = os
-        self.info.settings.arch_build = arch
 
